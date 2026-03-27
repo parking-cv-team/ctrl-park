736b921fe1c12f69ab7b2afbf1d20fcf5551d58b
OdinMeng <mengdino384@gmail.com>
2026-03-27 11:30:34 +0100

    added detect_frame.py to obtain raw detections (to be processed with the tracking part) and a script to test the detection. also added models weights

---
+++ processing/detect_frame.py
@@ -0,0 +1,24 @@
+import supervision as sv 
+import cv2 
+from ultralytics import YOLO
+
+def detect_frame_dual(
+        frame,
+        mcar_path: str = r'models_weights\best.pt', # path to the car detection model
+        mped_path: str = r'models_weights\yolov5su.pt', # path to the pedestrian detection model
+        arg_car: dict = {'conf': 0.75}, # arguments to the car detector
+        arg_ped: dict = {'conf': 0.4}, # arguments to the pedestrian detector
+    ) -> dict[str, sv.Detections]:
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    model_car = YOLO(mcar_path)
+    model_pedestrian = YOLO(mped_path)
+
+    detections_car = model_car.predict(frame_rgb, **arg_car)[0]
+    detections_pedestrians = model_pedestrian.predict(frame_rgb, classes=0, **arg_ped)[0]
+    
+    return {
+        'cars': sv.Detections.from_ultralytics(detections_car),
+        'pedestrians': sv.Detections.from_ultralytics(detections_pedestrians)
+    }
+
