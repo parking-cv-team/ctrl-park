f591124e6a748f9dd653ce58d16b274266df1a01
OdinMeng <mengdino384@gmail.com>
2026-03-30 11:38:14 +0200

    added 2 new fine tuned models, should be better but also heavier to run

--- processing/SCRIPT_detect_video.py
+++ processing/SCRIPT_detect_video.py
@@ -1,8 +1,17 @@
 import argparse
 import cv2
 import supervision as sv
-from .detect_frame import detect_frame_dual as detect
-from .detect_frame import load_models
+
+BASELINE_ = True
+
+if BASELINE_:
+    from .detect_frame import detect_frame_baseline as detect
+    from .detect_frame import load_baseline_model as load_model
+else:
+    from .detect_frame import detect_frame_dual as detect
+    from .detect_frame import load_models as load_models
+
+
 
 # RUN THIS BY WRITING THE FOLLOWING ON COMMAND PROMPT:
 # python -m processing.SCRIPT_detect_video --video <insert_video_path>
@@ -29,13 +38,25 @@ def main():
 
     frame_idx = 0
     
-    m_car, m_ped = load_models()
+    if BASELINE_:
+        m1 = load_model()
+    
+    else:
+        m1, m2 = load_models()
+
     while True:
         ret, frame = cap.read()
         if not ret:
             break
 
-        cars, peds = detect(frame, m_car, m_ped)
+        if BASELINE_:
+            raw = detect(frame, m1)
+
+        else:
+            raw = detect(frame, m1, m2)
+
+        cars = raw['cars']
+        peds = raw['pedestrian']
 
         annotated = box_annotator.annotate(frame.copy(), detections=cars)
         annotated = box_annotator.annotate(annotated, detections=peds)
